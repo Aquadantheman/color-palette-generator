408863ce1a80f2be61c6b2e52a0cd97391ade9e2
Aquadantheman <157748292+Aquadantheman@users.noreply.github.com>
2025-08-26 11:16:09 -0400

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,14 @@
+export type RGB = [number, number, number];
+
+export interface Swatch {
+  rgb: RGB;
+  hex: string;
+}
+
+export interface Analysis {
+  score: number;
+  label: string;
+  description: string;
+  bridgeColors: Swatch[];
+}
 
