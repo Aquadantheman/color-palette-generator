8edfc1a6e4916eaec1d4c6cc0fdfd92c0703808c
Aquadantheman <157748292+Aquadantheman@users.noreply.github.com>
2025-08-27 14:29:58 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,12 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc' // or '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   base: '/color-palettes/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-        assetFileNames: `assets/[name]-[hash][extname]`,
-        chunkFileNames: `assets/[name]-[hash].js`,
-        entryFileNames: `assets/[name]-[hash].js`,
-      }
-    }
+    assetsDir: 'assets'
   }
 })
