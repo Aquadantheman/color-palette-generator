683a7aa5c45c0b4713f5edb6c8021b53f0e39434
Aquadantheman <157748292+Aquadantheman@users.noreply.github.com>
2025-08-27 14:48:02 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 // vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/color-palettes/', // IMPORTANT for username.github.io/color-palettes/
+  base: '/color-palettes/',
 })
