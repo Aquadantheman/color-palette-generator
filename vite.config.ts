cb216369e879993b5b2ce5ce481479ad2eb1afa5
Aquadantheman <157748292+Aquadantheman@users.noreply.github.com>
2025-08-27 17:11:26 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   base: '/color-palettes/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  }
 })
